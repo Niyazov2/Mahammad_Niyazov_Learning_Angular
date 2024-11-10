f82a23b8559428767678337d669802df7f6b5954
Niyazov2 <mahammadniyazov1406@gmail.com>
2024-11-10 15:33:46 -0500

    modified app.component.html and added variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,6 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'mahammad_niyazov_learning_angular';
+  firstName = "Mahammad";
+  lastName = "Niyazov";
 }
