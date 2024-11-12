feca6e6b3fca4eed8be8d29f4c8b41e79ee60634
Niyazov2 <mahammadniyazov1406@gmail.com>
2024-11-11 19:28:05 -0500

    added in html for list item component and css styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Event } from './interface/event';
 import { NgFor, NgIf } from '@angular/common';
+import { EventListComponent } from "./event-list/event-list.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgFor, NgIf],
+  imports: [RouterOutlet, NgFor, NgIf, EventListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/event-list-item/event-list-item.component.ts
+++ src/app/event-list-item/event-list-item.component.ts
@@ -1,10 +1,11 @@
 import { NgIf } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { Event } from '../interface/event';
 
 @Component({
   selector: 'app-event-list-item',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './event-list-item.component.html',
   styleUrl: './event-list-item.component.css'
 })
