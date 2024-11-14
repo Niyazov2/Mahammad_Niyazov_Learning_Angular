fe648df880a0ae66698424c0a006958ce96e0648
Niyazov2 <mahammadniyazov1406@gmail.com>
2024-11-13 19:47:51 -0500

    created services and moved array out of event list component

--- src/app/event-list/event-list.component.ts
+++ src/app/event-list/event-list.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Event } from '../interface/event';
+import { eventList } from '../data/event-items';
 import { NgFor, NgClass } from '@angular/common';
 import { EventListItemComponent } from "../event-list-item/event-list-item.component";
 
@@ -11,55 +11,5 @@ import { EventListItemComponent } from "../event-list-item/event-list-item.compo
   styleUrl: './event-list.component.css'
 })
 export class EventListComponent {
- 
-  eventList: Event[] = [
-      {
-      id: 1,
-      nameOfEvent: "Windsor-Essex Tech Conference",
-      hostName: "Niyazov",
-      email: "niyazov@gmail.com",
-      imageUrl:"",
-      isAdmin: true
-      },
-      {
-        id: 2,
-        nameOfEvent: "Angular Conference 2024",
-        hostName: "Jane Doe",
-        email: "doe@gmail.com",
-        imageUrl:"",
-        isAdmin: false
-      },
-      {
-        id: 3,
-        nameOfEvent: "Artifical Intelligence Workshop",
-        hostName: "Usman",
-        email: "usman213@gmail.com",
-        imageUrl:"",
-        isAdmin:true
-      },
-      {
-        id: 4,
-        nameOfEvent: "Blockchain Expo",
-        hostName: "Michael",
-        email: "michael@example.com",
-        imageUrl: "",
-        isAdmin: false
-    },
-    {
-        id: 5,
-        nameOfEvent: "Marketing",
-        hostName: "Ahmed ",
-        email: "ahmed@example.com",
-        imageUrl: "",
-        isAdmin: true
-    },
-    {
-        id: 6,
-        nameOfEvent: "Data Science Meetup",
-        hostName: "Ashley",
-        email: "ashley@example.com",
-        imageUrl: "",
-        isAdmin: true
-    }
-  ]
+eventList = eventList;
 }
