ab46ea83999dc5aa34831c7cad77895a71630292
Niyazov2 <mahammadniyazov1406@gmail.com>
2024-11-13 20:49:14 -0500

    completed assignment 4 with bonus marks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Event } from './interface/event';
 import { NgFor, NgIf } from '@angular/common';
 import { EventListComponent } from "./event-list/event-list.component";
+import { EventListItemComponent } from "./event-list-item/event-list-item.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgFor, NgIf, EventListComponent],
+  imports: [RouterOutlet, NgFor, NgIf, EventListComponent, EventListItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/event-list/event-list.component.ts
+++ src/app/event-list/event-list.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 import { Event } from '../interface/event';
-import { NgFor, NgClass } from '@angular/common';
+import { NgFor, NgClass, NgIf } from '@angular/common';
 import { EventListItemComponent } from "../event-list-item/event-list-item.component";
 import { EventServiceService } from '../services/event-service.service';
 
 @Component({
   selector: 'app-event-list',
   standalone: true,
-  imports: [NgFor, EventListItemComponent, NgClass],
+  imports: [NgFor, EventListItemComponent, NgClass, NgIf],
   templateUrl: './event-list.component.html',
   styleUrl: './event-list.component.css'
 })
 export class EventListComponent {
+
+selectedEvent?: Event;
+
  eventList: Event[] = [];
 
 constructor(private EventServiceService: EventServiceService) {}
@@ -20,5 +23,14 @@ ngOnInit(): void{
   this.EventServiceService.getEvents().subscribe((data) =>{
     this.eventList = data;
   });
+
+  this.selectEvent(3);
+}
+
+
+selectEvent(id:number){
+  this.EventServiceService.getEventsById(id).subscribe((selectedData) => {
+    this.selectedEvent = selectedData;
+  })
 }
 }
