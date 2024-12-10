4de254337b750b1626239089d706d0d4d1a48365
Niyazov2 <mahammadniyazov1406@gmail.com>
2024-12-10 15:50:47 -0500

    added lazy loadin

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,20 @@ import { PageNotFoundComponent } from './app/page-not-found/page-not-found.compo
 
 const routes: Routes = [
   {path: '', redirectTo: '/event', pathMatch:'full'},
-  {path: 'event', component: EventListComponent},
-  {path: 'modify-event', component: ModifyItemListComponent},
+  {
+    path: 'event',
+    loadComponent: () =>
+      import('./app/event-list/event-list.component').then(
+        (m) => m.EventListComponent
+      ),
+  },
+  {
+    path: 'modify-event',
+    loadComponent: () =>
+      import('./app/modify-item-list/modify-item-list.component').then(
+        (m) => m.ModifyItemListComponent
+      ),
+  },
   {path: '**', component: PageNotFoundComponent}]
 
 bootstrapApplication(AppComponent, {
